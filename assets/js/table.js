443e1aca10f298eff3612697acb7f9e24757d596
yn-tsuy <71456035+yn-tsuy@users.noreply.github.com>
2020-10-01 13:31:08 -0300

    Update table.js

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,12 +1,6 @@
-$(document).ready(function() {
+$('document').ready(function () {
     $('#tbItens').DataTable();
-});
-$(document).ready(function() {
     $('#tbConsulta').DataTable();
-});
-$(document).ready(function() {
     $('#tbConsultaReq').DataTable();
-});
-$(document).ready(function() {
     $('#tbReq').DataTable();
 });
